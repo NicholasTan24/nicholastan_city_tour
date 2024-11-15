f905d92871261c08e6378f4375db04d017a82f3c
NicholasTan24 <nicholastan_2327240005@mhs.mdp.ac.id>
2024-11-15 18:25:05 +0700

    Perbaikan program UTS
    Memperbaiki program UTS yang kacau

--- nicholastan_city_tour/lib/HomeScreen.dart
+++ nicholastan_city_tour/lib/HomeScreen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-class Homescreen extends StatelessWidget{
+class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
 
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,37 +17,56 @@ class Homescreen extends StatelessWidget{
         title: Text('City Tour'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         child: SingleChildScrollView(
-          child: SizedBox(
-            child: Column(
-              children: [
-                SizedBox(height: 16),
-                SingleChildScrollView(
-                    child: Row(
-                      children: [
-                        ClipRRect(
-                            child: Image.network(
-                              height: 300,
-                                width: 150,
-                                'https://picsum.photos/id/162/300/200'
-                                    'https://picsum.photos/id/164/300/200'
-                                    'https://picsum.photos/id/174/300/200'
-
-                            ),
-                        ),
-                        Column(children: [
-                          Text("Explore the charm of our city's hidden gems and embark on a journey to discover the rich cultural heritage, breathtaking landscapes, and unique experiences that await you. From historic landmarks to vibrant markets and serene natural wonders, our city offers a tapestry of experiences that will captivate your senses. Join us in exploring the beauty of our city and create memories that will last a lifetime. Come and visit our city's treasures today.",
-                              textAlign: TextAlign.justify)
-                        ],)
-                      ],
-                    )
-                )
-              ],
-            ),
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                          'https://picsum.photos/id/162/300/200',
+                          fit: BoxFit.cover,
+                          height: 300,
+                          width: 150,
+                          )),
+                  SizedBox(width: 8),
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                          'https://picsum.photos/id/164/300/200',
+                          fit: BoxFit.cover,
+                          height: 300,
+                          width: 150)),
+                  SizedBox(width: 8),
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                          'https://picsum.photos/id/174/300/200',
+                          fit: BoxFit.cover,
+                          height: 300,
+                          width: 150)),
+                ]),
+              ),
+              SizedBox(height: 10),
+              Text('Explore, Enjoy, Experience',
+                  style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              Text("Explore the charm of our city's hidden gems and embark on a journey to discover the rich cultural heritage, breathtaking landscapes, and unique experiences that await you. From historic landmarks to vibrant markets and serene natural wonders, our city offers a tapestry of experiences that will captivate your senses. Join us in exploring the beauty of our city and create memories that will last a lifetime. Come and visit our city's treasures today.",
+              textAlign: TextAlign.justify),
+              SizedBox(height: 10),
+              Image.network('https://picsum.photos/id/162/200/300',
+                fit: BoxFit.cover, width: double.infinity, height: 250),
+              SizedBox(height: 10),
+              Text('Blue body of water'),
+            ],
           ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
--- nicholastan_city_tour/lib/main.dart
+++ nicholastan_city_tour/lib/main.dart
@@ -22,12 +22,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => Homescreen(key: key);
-}
 
